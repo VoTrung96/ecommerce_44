120f4b10b93637461222a3922b3eff9cd78b243c
VoTrung96 <votiensy240196@gmail.com>
2018-05-05 08:54:41 +0700

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :load_categories, only: %i(new edit)
+  before_action :logged_in_user, only: %i(edit update)
   before_action :find_user, only: %i(edit update)
+  before_action :correct_user, only: %i(edit update)
+  before_action :load_categories, only: %i(new edit)
 
   def new
     @user = User.new
@@ -9,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "flash.wellcome"
       redirect_to edit_user_path @user
     else
@@ -45,5 +48,16 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :phone_number, :address,
       :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "flash.login_danger"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
   end
 end
