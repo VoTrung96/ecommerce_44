ca299a6e04143dc496d6c153b9b77c141c033fbd
VoTrung96 <votiensy240196@gmail.com>
2018-05-05 08:54:41 +0700

    update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :load_categories, only: :new
+  before_action :load_categories, only: %i(new edit)
+  before_action :find_user, only: %i(edit update)
 
   def new
     @user = User.new
@@ -8,16 +9,37 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      redirect_to root_path
+      flash[:success] = t "flash.wellcome"
+      redirect_to edit_user_path @user
     else
+      flash[:danger] = t "flash.danger"
       render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "flash.update_profile_success"
+      redirect_to edit_user_path @user
+    else
+      flash[:danger] = t "flash.danger"
+      render :edit
     end
   end
 
   private
 
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user.present?
+    flash[:danger] = t "flash.user_invalid"
+    redirect_to root_path
+  end
+
   def load_categories
-    @categories = Category.get_sub_categories Settings.category.category_lv0
+    @categories = Category.get_parent_categories
   end
 
   def user_params
